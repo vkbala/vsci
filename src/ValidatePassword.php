01e68386728171548d2ee1e0d15ebeaa5e6f8939
unknown <balaka@BGLRLMSSC005.ap.thmulti.com>
2015-05-25 23:39:05 +0530

    adds validate password class

---
+++ src/ValidatePassword.php
@@ -0,0 +1,12 @@
+<?php
+
+class ValidatePassword {
+
+	const MIN_LENGTH = 6;
+	const MAX_LENGTH = 28;
+
+	public function validLength($password) {
+		$passLength = strlen($password);
+		return $passLength >= self::MIN_LENGTH && $passLength <= self::MAX_LENGTH;
+	}
+}
---
+++ tests/ValidatePasswordTest.php
@@ -0,0 +1,10 @@
+<?php
+
+class ValidatePasswordTest extends PHPUnit_Framework_TestCase {
+
+	public function testValidateLength() {
+		$valPass = new ValidatePassword();
+		$this->assertFalse($valPass->validLength('1234'));
+	}	
+	
+}
