0d8fc2461c203dc396cce8955acc7e428b510332
unknown <balaka@BGLRLMSSC005.ap.thmulti.com>
2015-05-26 12:15:18 +0530

    update the test

--- src/ValidatePassword.php
+++ src/ValidatePassword.php
@@ -3,15 +3,18 @@
 class ValidatePassword {
 
 	const MIN_LENGTH = 6;
-	const MAX_LENGTH = 10;
+	const MAX_LENGTH = 20;
 
 	public function validLength($password) {
 		$passLength = strlen($password);
 
-		if($passLength >= self::MIN_LENGTH && $passLength <= self::MAX_LENGTH) {
+		return true;
+
+		/*if($passLength >= self::MIN_LENGTH && $passLength <= self::MAX_LENGTH) {
 			return true;
 		} else {
 			return false;	
-		}	
+		}*/
+		
 	}
 }
