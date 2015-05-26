fe7e1e19f60617e2e2b064c25d3ac7ce7f604dd0
unknown <balaka@BGLRLMSSC005.ap.thmulti.com>
2015-05-26 13:38:23 +0530

    Update with two function with failed

--- tests/ValidatePasswordTest.php
+++ tests/ValidatePasswordTest.php
@@ -5,13 +5,13 @@ class ValidatePasswordTest extends PHPUnit_Framework_TestCase {
 	public function testValidateLength() {
 		$valPass = new ValidatePassword();
 		//fwrite(STDOUT, __METHOD__ . "\n");
-		$this->assertTrue($valPass->validLength('Password@123'));
+		$this->assertTrue($valPass->validLength('Passw'));
 	}	
 
 	public function testValidatePassword() {
 		$valPass = new ValidatePassword();
 		//fwrite(STDOUT, __METHOD__ . "\n");
 		//$this->assertFalse($valPass->validPassword('Password@123'));
-		$this->assertTrue($valPass->validPassword('Password@123'));
+		$this->assertTrue($valPass->validPassword('Pass'));
 	}		
 }
